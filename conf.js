f8a36c37109d8ed656ebaeec45c326239ae81919
pvan <pvan@bach.cz>
2026-05-27 16:52:37 +0200

    Added advanced message actions and PDF export in SideQuery extension

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
 
 export const EXTENSION_NAME = "SillyTavern-SideQuery";
 export const EXTENSION_PATH = "third-party/SillyTavern-SideQuery";
-export const VERSION = "0.1.0";
+export const VERSION = "1.0.0";
 export const MODULE_NAME = "enerccio_sidequery";
